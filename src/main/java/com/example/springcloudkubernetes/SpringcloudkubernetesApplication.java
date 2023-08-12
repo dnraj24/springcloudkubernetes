6280092a064133a62d1ce8703db0d8bcc3a75e52
Devarshetty <ndevarshetty@skipcart.com>
2023-08-12 00:04:10 -0500

    intial

---
+++ src/main/java/com/example/springcloudkubernetes/SpringcloudkubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudkubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudkubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudkubernetesApplication.class, args);
+    }
+
+}
